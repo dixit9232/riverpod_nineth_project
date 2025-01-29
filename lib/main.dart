5fd55a3fe5a75ded73cbe7634c7087e62cb8e0f4
Dixit Pambhar <dixitpambhar9232@gmail.com>
2025-01-29 19:54:36 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_nineth_project/home_screen.dart';
 
 void main() {
-  
-
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
+
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-         
+
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
+            brightness: Brightness.dark,
           ),
           useMaterial3: true,
         ),
